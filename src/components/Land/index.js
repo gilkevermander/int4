8e0dff32a5f4274915528f2636bb4acf618d0c49
Lisebeth Duyvejonck <lisebeth.duyvejonck@student.howest.be>
2020-06-15 21:48:50 +0200

    CSS vervolg

--- src/components/Land/index.js
+++ src/components/Land/index.js
@@ -3,13 +3,13 @@ import { Form, Button } from 'semantic-ui-react';
 import style from "./Land.module.css";
 
 
-const Land = ({nextStep, values, setLand, landStore, prevStep}) => {
+const Land = ({ nextStep, values, setLand, landStore, prevStep }) => {
 
   const [error, setError] = useState("");
 
   const saveAndContinue = (e) => {
     e.preventDefault()
-    if(values.land === "" ){
+    if (values.land === "") {
       setError("duid een land aan")
     } else {
       nextStep()
@@ -26,56 +26,60 @@ const Land = ({nextStep, values, setLand, landStore, prevStep}) => {
 
   return (
     <>
-    <div className={style.procesbar}>
+      <div className= {style.header}>
+        <Button onClick={back} className={style.back}><p className={style.back__text}>&lt;</p></Button>
+        <div className={style.procesbar}>
 
-      <div className={style.procesbar_lijn1}>
-        <div className={style.procesbar__item}>
-          <p className={style.item__number}>1</p>
-          <p className={style.item__text}>Verhaal</p>
+          <div className={style.procesbar_lijn1}>
+            <div className={style.procesbar__item}>
+              <p className={style.item__number}>1</p>
+              <p className={style.item__text}>Verhaal</p>
+            </div>
+          </div>
+
+          <div className={style.procesbar_lijn2}>
+            <div className={style.procesbar__item1}>
+              <p className={style.item__number}>2</p>
+              <p className={style.item__text}>land</p>
+            </div>
+          </div>
+
+          <div className={style.procesbar_lijn3}>
+            <div className={style.procesbar__item}>
+              <p className={style.item__number}>3</p>
+              <p className={style.item__text}>Opname</p>
+            </div>
+          </div>
+
+          <div className={style.procesbar_lijn4}>
+            <div className={style.procesbar__item}>
+              <p className={style.item__number}>4</p>
+              <p className={style.item__text}>Souvenir</p>
+            </div>
+          </div>
+
+          <div className={style.procesbar__item}>
+            <p className={style.item__number}>5</p>
+            <p className={style.item__text}>Gegevens</p>
+          </div>
         </div>
       </div>
 
-      <div className={style.procesbar_lijn2}>
-        <div className={style.procesbar__item1}>
-          <p className={style.item__number}>2</p>
-          <p className={style.item__text}>land</p>
-        </div>
-      </div>
+      <Form color='blue' >
+        <h1 className={style.vraag}>Kies het land van jouw herinnering</h1>
+        <p>{error}</p>
+        <Form.Field>
+          <select name="land" id="land" onChange={e => setLand(e.currentTarget.value)}>
+            <option key="niks" id="land" name="land" value="" >kies een optie</option>
+            {landStore.lands.map(land => (
+              <option key={land.id} id="land" name="land" value={land.title} >{land.title}</option>
+            ))}
+          </select>
+        </Form.Field>
 
-      <div className={style.procesbar_lijn3}>
-        <div className={style.procesbar__item}>
-          <p className={style.item__number}>3</p>
-          <p className={style.item__text}>Opname</p>
-        </div>
-      </div>
+        <Button onClick={saveAndContinue} className={style.next}><p className={style.next__text}>Volgende</p> </Button>
+      </Form>
 
-      <div className={style.procesbar_lijn4}>
-        <div className={style.procesbar__item}>
-          <p className={style.item__number}>4</p>
-          <p className={style.item__text}>Souvenir</p>
-        </div>
-      </div>
-
-      <div className={style.procesbar__item}>
-        <p className={style.item__number}>5</p>
-        <p className={style.item__text}>Gegevens</p>
-      </div>
-    </div>
-
-    <Form color='blue' >
-      <h1 className={style.vraag}>Kies het land van jouw herinnering</h1>
-      <p>{error}</p>
-      <Form.Field>
-        <select name="land" id="land" onChange={e => setLand(e.currentTarget.value)}>
-        <option key="niks" id="land" name="land" value="" >kies een optie</option>
-          {landStore.lands.map(land => (
-            <option key={land.id} id="land" name="land" value={land.title} >{land.title}</option>
-          ))}
-        </select>
-      </Form.Field>
-      <Button onClick={back} className={style.back}><p className={style.back__text}>&lt;</p></Button>
-      <Button onClick={saveAndContinue} className={style.next}><p className={style.next__text}>Volgende</p> </Button>
-    </Form>
     </>
   )
 }
