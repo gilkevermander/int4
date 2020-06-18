f0f7b1ecbfcafd573a4272152e25961efb19ba5e
alieseechaute <alies.eechaute@student.howest.be>
2020-06-18 23:29:16 +0200

    navigatiebar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./NavBar.module.css";
 import { ReactComponent as MessageLogo } from "./message.svg";
-import { ReactComponent as ScanLogo } from "./scan.svg";
+import scan from "../../assets/img/scan.png";
 import { ReactComponent as HomeLogo } from "./home.svg";
 import { NavLink } from "react-router-dom";
 import { ROUTES } from "../../consts";
@@ -10,22 +10,22 @@ const NavBar = () => {
   return (
     <nav className={style.navbar}>
       <ul className={style.list}>
-        <li>
-          <NavLink className={style.button} to={ROUTES.home}>
+        <li className={style.list__item}>
+          <NavLink activeClassName={style.active} className={style.button}  to={ROUTES.home}>
             <HomeLogo fill="white" />
-            <span >Home</span>
+            <span className={style.list_text}>Home</span>
           </NavLink>
         </li>
-        <li>
-          <NavLink className={style.button} to={ROUTES.qr}>
-            <ScanLogo fill="currentColor" />
-            <span>scan</span>
+        <li className={style.list__item}>
+          <NavLink className={[style.button__scan]} activeClassName={style.active} to={ROUTES.qr}>
+            <img src={scan} alt="scan"/>
+            <span className={ style.list_text}>Scan je verhaal</span>
           </NavLink>
         </li>
-        <li>
-          <NavLink className={style.button} to={ROUTES.chat}>
+        <li className={style.list__item}>
+          <NavLink className={style.button} activeClassName={style.active} to={ROUTES.chat}>
             <MessageLogo fill="white" />
-            <span>Messages</span>
+            <span className={style.list_text}>Berichten</span>
           </NavLink>
         </li>
       </ul>
