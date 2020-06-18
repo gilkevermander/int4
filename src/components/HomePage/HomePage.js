c48fff8a6e4f63014984621973e990954418ee14
alieseechaute <alies.eechaute@student.howest.be>
2020-06-19 00:37:56 +0200

    home pagina af

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,6 +3,9 @@ import InfoHeader from "../InfoHeader/InfoHeader";
 import style from "./HomePage.module.css";
 import { NavLink } from "react-router-dom";
 import { ROUTES } from "../../consts/index";
+import luisteren from "../../assets/img/luisteren.png";
+import cabine from "../../assets/img/cabine.png"
+import verhalen from "../../assets/img/verhalen.png"
 
 const HomePage = () => {
   const innerRef = useRef();
@@ -12,21 +15,30 @@ const HomePage = () => {
   };
   return (
     <section className={style.breedte}>
-      <InfoHeader title={"Kabien"} className={style.title}/>
+      <InfoHeader title={"Kabien"} className={style.title} />
       <div className={style.headwrapper}>
-      <div className={style.wrapper}>
-      <NavLink className={style.add} to={ROUTES.qr}>
-        <h2 className={style.link}>Luister naar jouw souvenir</h2>
-      </NavLink>
-      <NavLink onClick={getLocation} className={style.add} to="/locatie">
-        <h2 className={style.link}>Neem jouw herinnering op</h2>
-      </NavLink>
-      </div>
-      <div className={style.wrapper}>
-      <NavLink className={style.add} to={ROUTES.kaart}>
-        <h2 className={style.link}>Ontdek verhalen van KABIEN</h2>
-      </NavLink>
-      </div>
+        <div className={style.wrapper}>
+          <NavLink className={style.add} to={ROUTES.qr}>
+            <div className={style.link}>
+              <img src={luisteren} alt="luisteren" className={style.image}/>
+              <h2 className={style.linktext}>Luister naar jouw souvenir</h2>
+            </div>
+          </NavLink>
+          <NavLink onClick={getLocation} className={style.add} to="/locatie">
+            <div className={style.link}>
+              <img src={cabine} alt="cabine" className={style.image} />
+              <h2 className={style.linktext}>Neem jouw herinnering op</h2>
+            </div>
+          </NavLink>
+        </div>
+        <div className={style.wrapper}>
+          <NavLink className={style.add} to={ROUTES.kaart}>
+            <div className={style.link}>
+              <img src={verhalen} alt="verhalen" className={style.image} />
+              <h2 className={style.linktext}>Ontdek verhalen van KABIEN</h2>
+            </div>
+          </NavLink>
+        </div>
       </div>
 
     </section>
