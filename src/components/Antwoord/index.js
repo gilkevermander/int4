c54abb74541bbf3e45f5d45e45e74bf231907ce4
alieseechaute <alies.eechaute@student.howest.be>
2020-06-19 21:03:29 +0200

    antwoord pagina gestylet

--- src/components/Antwoord/index.js
+++ src/components/Antwoord/index.js
@@ -1,19 +1,61 @@
 import React from "react";
-import InfoHeader from "../InfoHeader/InfoHeader";
-import style from "./Antwoord.module.css"
+import ContentHeader from "../ContentHeader/ContentHeader";
+import style from "./Antwoord.module.css";
+import vs from "../../assets/img/vs.png";
+import belgie from "../../assets/img/belgie.png"
+import italie from "../../assets/img/italie.png"
+import japan from "../../assets/img/japan.png"
 
 const Antwoord = ({antwoord}) => {
 
 
-  
-  return (
-    <section className={style.container}>
-      <InfoHeader title={"quiz"} />
+  if (antwoord === "Italië") {
+    return (
+      <section className={style.container}>
+      <ContentHeader title={"Resultaat"} />
+      <img src={italie} alt="italie" className={style.img}/>
+      <p className={style.favo}> Jouw favoriete land is Italië</p>
+      <p className={style.uitleg}>Jij geniet van een glaasje wijn met een lekkere pasta daarbij! Een echte levensgenieter</p>
+      <p className={style.actie}>Ga in KABIEN en vertel jouw verhaal!</p>
       
-      <p>{antwoord}</p>
+      </section>
+    )
+  } if (antwoord === "VS") {
+    return (
+      <section className={style.container}>
+      <ContentHeader title={"Resultaat"} />
+      <img src={vs} alt="VS" className={style.img}/>
+      <p className={style.favo}> Jouw favoriete land is de Verenigde Staten</p>
+      <p className={style.uitleg}>Ow yeah! Jij bent een bezige bij! Nieuwe dingen ontdekken en interessante mensen ontmoeten is jouw favoriete bezigheid! You go!</p>
+      <p className={style.actie}>Ga in KABIEN en vertel jouw verhaal!</p>
       
-    </section>
-  );
+      </section>
+    )
+  } if (antwoord === "België") {
+    return (
+      <section className={style.container}>
+      <ContentHeader title={"Resultaat"} />
+      <img src={belgie} alt="belgie" className={style.img}/>
+      <p className={style.favo}> Jouw favoriete land is België</p>
+      <p className={style.uitleg}>Home sweet home is jouw levensmotto! Nergens beter dan thuis. Het lekkerste eten, het meeste comfort! We geven je volledig gelijk!</p>
+      <p className={style.actie}>Ga in KABIEN en vertel jouw verhaal!</p>
+      
+      </section>
+    )
+  } if (antwoord === "Japan") {
+    return (
+      <section className={style.container}>
+      <ContentHeader title={"Resultaat"} />
+      <img src={japan} alt="japan" className={style.img}/>
+      <p className={style.favo}> Jouw favoriete land is Japan</p>
+      <p className={style.uitleg}>Geen enkel land heeft voor jou nog geheimen! Jij staat open voor nieuwe ontdekkingen! Wanderlust past dan ook perfect bij jou!</p>
+      <p className={style.actie}>Ga in KABIEN en vertel jouw verhaal!</p>
+      
+      </section>
+    )
+  }
+
+
 };
 
 export default Antwoord;
