a2cb8c43140faf049bb1534664e5977119fd0bfd
alieseechaute <alies.eechaute@student.howest.be>
2020-06-20 12:11:02 +0200

    navbar aanpassingen

--- src/components/Antwoord/index.js
+++ src/components/Antwoord/index.js
@@ -6,6 +6,8 @@ import belgie from "../../assets/img/belgie.png"
 import italie from "../../assets/img/italie.png"
 import japan from "../../assets/img/japan.png"
 import NavBarSouvenir from "../NavBarSouvenir/index";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../consts/index";
 
 const Antwoord = ({antwoord}) => {
 
@@ -18,7 +20,9 @@ const Antwoord = ({antwoord}) => {
       <p className={style.favo}> Jouw favoriete land is Italië</p>
       <p className={style.uitleg}>Jij geniet van een glaasje wijn met een lekkere pasta daarbij! Een echte levensgenieter</p>
       <p className={style.actie}>Ga in KABIEN en vertel jouw verhaal!</p>
-      <NavBarSouvenir/>
+      <Link className={style.button} to={ROUTES.souvenirWall}>
+            <span className={style.button__text}>Scan een ander souvenir</span>
+      </Link>
       </section>
     )
   } if (antwoord === "VS") {
@@ -29,7 +33,9 @@ const Antwoord = ({antwoord}) => {
       <p className={style.favo}> Jouw favoriete land is de Verenigde Staten</p>
       <p className={style.uitleg}>Ow yeah! Jij bent een bezige bij! Nieuwe dingen ontdekken en interessante mensen ontmoeten is jouw favoriete bezigheid! You go!</p>
       <p className={style.actie}>Ga in KABIEN en vertel jouw verhaal!</p>
-      <NavBarSouvenir/>
+      <Link className={style.button} to={ROUTES.souvenirWall}>
+            <span className={style.button__text}>Scan een ander souvenir</span>
+      </Link>
       </section>
     )
   } if (antwoord === "België") {
@@ -40,7 +46,9 @@ const Antwoord = ({antwoord}) => {
       <p className={style.favo}> Jouw favoriete land is België</p>
       <p className={style.uitleg}>Home sweet home is jouw levensmotto! Nergens beter dan thuis. Het lekkerste eten, het meeste comfort! We geven je volledig gelijk!</p>
       <p className={style.actie}>Ga in KABIEN en vertel jouw verhaal!</p>
-      <NavBarSouvenir/>
+      <Link className={style.button} to={ROUTES.souvenirWall}>
+            <span className={style.button__text}>Scan een ander souvenir</span>
+      </Link>
       </section>
     )
   } if (antwoord === "Japan") {
@@ -51,7 +59,9 @@ const Antwoord = ({antwoord}) => {
       <p className={style.favo}> Jouw favoriete land is Japan</p>
       <p className={style.uitleg}>Geen enkel land heeft voor jou nog geheimen! Jij staat open voor nieuwe ontdekkingen! Wanderlust past dan ook perfect bij jou!</p>
       <p className={style.actie}>Ga in KABIEN en vertel jouw verhaal!</p>
-      <NavBarSouvenir/>
+      <Link className={style.button} to={ROUTES.souvenirWall}>
+            <span className={style.button__text}>Scan een ander souvenir</span>
+      </Link>
       </section>
     )
   }
--- src/components/NavBarSouvenir/index.js
+++ src/components/NavBarSouvenir/index.js
@@ -9,9 +9,9 @@ const NavBarSouvenir = () => {
     <nav className={style.navbar}>
       <ul className={style.list}>
         <li className={style.list__item}>
-          <NavLink className={style.button__scan} activeClassName={style.active} to={ROUTES.qr} >
+          <NavLink className={style.button__scan} activeClassName={style.active} to={ROUTES.souvenirWall} >
             <img src={scan} alt="scan" width="82" height="82" className={style.img}/>
-            <span className={ style.list_text}>Scan je verhaal</span>
+            <span className={ style.list_text}>Scan je souvenir</span>
           </NavLink>
         </li>
       </ul>
